88bcddad90d7b14bea7204e5dfef689ba29fec88
yoshida <yoshida@crux-lab.co.jp>
2017-10-03 19:48:38 +0900

    master

--- develop/js/_common/module/scroll_header_effect.js
+++ develop/js/_common/module/scroll_header_effect.js
@@ -4,16 +4,20 @@ let scrollHeaderEffect = (function() {
 	let $window         = $(window);
 	let $header         = $('#header');
 	let headerHeight    = $header.height();
+	var scrollPosi      = $window.scrollTop();
 	const SHOW_POSITION = 200;
+	judgment(scrollPosi);
 
 	$window.on('scroll.scrollEffectCommon', function() {
 		let currentPostion = $(this).scrollTop();
-
+		judgment(currentPostion);
+	});
+	function judgment(currentPostion){
 		if(currentPostion > headerHeight){
 			$header.addClass('is-short');
 		}else{
 			$header.removeClass('is-short');
 		}
-	});
+	}
 }());
 export default scrollHeaderEffect;
