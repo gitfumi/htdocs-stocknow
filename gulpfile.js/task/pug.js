0da30a3ed8d348c9c1754fd7da80be59f7529ecd
gitfumi <deep.blue.f23@gmail.com>
2019-02-27 10:42:06 +0900

    2019.02.27 pug調整

--- gulpfile.js/task/pug.js
+++ gulpfile.js/task/pug.js
@@ -42,7 +42,8 @@ module.exports = {
 		// pugのコンパイル
 		.pipe($.pug({
 			basedir: config.root.src,
-			pretty: '\t'
+			pretty: '\t',
+			doctype: 'html'
 		}))
 		.pipe($.gulp.dest(config.root.src));
 	},
